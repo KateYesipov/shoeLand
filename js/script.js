d32a78032e87b26f45487afa51d52f5f02b8c9f6
Kate Yesipova <katerina.yesipova@gmail.com>
2023-09-06 18:39:25 +0300

    add 4 block

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+function OpenMenu() {
+    let OpenBurger = document.querySelector(".header__burger"),
+        menuBurger = document.querySelector(".burger"),
+        backgroundMenu = document.querySelector(".menu-mobile");
+    OpenBurger.addEventListener
+    menuBurger.classList.toggle("active"), backgroundMenu.classList.toggle("active");
+
+}
